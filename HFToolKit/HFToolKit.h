e802aad324274325b2dcfa40e5d9dbcbf71240d0
crazylhf <linhuafeng@yy.com>
2015-07-27 22:01:17 +0800

    add HFTookKit project

---
+++ HFToolKit/HFToolKit.h
@@ -0,0 +1,13 @@
+//
+//  HFToolKit.h
+//  HFToolKit
+//
+//  Created by crazylhf on 15/7/27.
+//  Copyright (c) 2015年 crazylhf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HFToolKit : NSObject
+
+@end
---
+++ HFToolKit/HFToolKit.m
@@ -0,0 +1,13 @@
+//
+//  HFToolKit.m
+//  HFToolKit
+//
+//  Created by crazylhf on 15/7/27.
+//  Copyright (c) 2015年 crazylhf. All rights reserved.
+//
+
+#import "HFToolKit.h"
+
+@implementation HFToolKit
+
+@end
